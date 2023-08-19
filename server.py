5ccc43ebfb5f303176170a0507d0c151388b988d
soumyej102 <soumyej102@gmail.com>
2023-08-19 14:26:40 +0530

    Made changes to server.py file

--- server.py
+++ server.py
@@ -1,19 +1,41 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    try:
+        data = request.get_json()
+        first = data.get('first')
+        second = data.get('second')
+
+        if first is None or second is None:
+            return jsonify({"error": "Missing 'first' or 'second' parameter"}), 400
+
+        result = first + second
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        data = request.get_json()
+        first = data.get('first')
+        second = data.get('second')
+
+        if first is None or second is None:
+            return jsonify({"error": "Missing 'first' or 'second' parameter"}), 400
+
+        result = first - second
+        return jsonify({"result": result}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
